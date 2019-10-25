3a110f81eccd89c0dc6b5227e56e9634df2fcf9e
ramkumar <ramkumar714ag@gmail.com>
2019-10-25 17:41:05 +0530

    UserInterface

---
+++ das/frontend/userinterface/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import TopNavBar from './components/TopNavBar'
+import Content from './components/Content'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+        <TopNavBar />
+        <Content />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
