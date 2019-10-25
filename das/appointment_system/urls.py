84894bfe6dd3cc1089d05df9c33798c0ace7c185
ramkumar <ramkumar714ag@gmail.com>
2019-10-25 16:04:33 +0530

    api & db

--- das/appointment_system/urls.py
+++ das/appointment_system/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from appointment_system.views import DoctorsList, AppointmentList
 
 urlpatterns = [
-    path('doctorDetails/', DoctorsList.as_view(), name='DoctorsList'),
-    path('appointmentDetails/', AppointmentList.as_view(), name='AppointmentList')
+    path('doctorDetails/', DoctorsList.as_view()),
+    path('appointmentDetails/', AppointmentList.as_view())
 ]
